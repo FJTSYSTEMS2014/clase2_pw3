eebff01a3dec3360cd31c42c794609a7303cdfd5
Franck_Tscherig <fjtsystemsg@gmail.com>
2021-07-13 19:25:11 -0300

    :)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 import charactersInfo from "./data/marvel/characters.json";
-import comicsInfo from "./data/marvel/comics.json";
+
 import DetalleDeHeroe from "./components/DetalleDeHeroe";
-import ListaDeLinks from "./components/ListaDeLinks";
+
 
 export default function App() {
   console.log(charactersInfo.data.results[0]);
@@ -11,8 +11,12 @@ export default function App() {
 
      <div><h1>Caracteres de Marvel</h1>
       <DetalleDeHeroe id="uno" heroe={charactersInfo.data.results[3]} /></div> 
-      <div><h1>hhhh</h1>
+      <div><h1>Marvel1</h1>
       <DetalleDeHeroe id="dos" heroe={charactersInfo.data.results[1]} />
+  
+        </div>
+        <div><h1>Marvel 2</h1>
+      <DetalleDeHeroe id="dos" heroe={charactersInfo.data.results[2]} />
   
         </div>
     </div>
